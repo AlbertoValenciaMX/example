223bceb648b9e28b6065fcdb2ebe75c7372ac87b
Alberto Valencia <rdz.val.alberto@gmail.com>
2021-09-21 13:10:50 -0500

    Ejercicio 28 terminado

--- python/problema28.py
+++ python/problema28.py
@@ -6,16 +6,16 @@ xE = [0.20,0.40,0.25,0.15]
 mE = np.zeros(4)
 mTE = 0
 for i in range(0,4):
-    mE[i] = (nE * xE[i]) * pm[i]
+    mE[i] = np.dot(np.dot(nE,xE[i]),pm[i])
     mTE = mTE + mE[i]
 
-print("Composición inicial= ",mE)
-print("Flujo másico inicial= ",mTE)
+print("Composición inicial= " + str(mE))
+print("Flujo másico inicial= " + str(mTE))
 Acoef = [[0.35, 0.16, 0.21, 0.01], [0.54, 0.42, 0.54, 0.1], [0.04, 0.24, 0.1, 0.65], [0.07, 0.18, 0.15, 0.24]]
-Bflu = [[mE[1]], [mE[2]], [mE[3]], [mE[4]]]
+Bflu = [[mE[0]], [mE[1]], [mE[2]], [mE[3]]]
 FsF = np.dot(np.linalg.inv(Acoef), Bflu)
 print("D1,B1,D2,B2:"+str(FsF))
-FsE = [FsF[1] + FsF[2],FsF[3] + FsF[4]]
+FsE = [FsF[0] + FsF[1],FsF[2] + FsF[3]]
 print("D, B:"+str(FsE))
 xD = np.zeros(4)
 xB = np.zeros(4)
@@ -24,8 +24,8 @@ xB1 = [0.16, 0.42, 0.24, 0.18]
 xD2 = [0.21, 0.54, 0.10, 0.15]
 xB2 = [0.01, 0.1, 0.65, 0.24]
 for i in range(0,4):
-    xD[i] = ((FsF[1] * xD1[i] + FsF[2] * xB1[i])) / FsE[1]
-    xB[i] = ((FsF[3] * xD2[i] + FsF[4] * xB2[i])) / FsE[2]
+    xD[i] = ((np.dot(FsF[0], xD1[i]) + np.dot(FsF[1], xB1[i]))) / FsE[0]
+    xB[i] = ((np.dot(FsF[2], xD2[i]) + np.dot(FsF[3], xB2[i]))) / FsE[1]
 
 print("Fracción en D: "+ str(xD))
 print("Fracción en B: "+ str(xB))
