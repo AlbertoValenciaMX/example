a41be773b4d4904366df52e945dfe217a739a7c8
Alberto Valencia <rdz.val.alberto@gmail.com>
2021-09-21 18:06:48 -0500

    Ejercicio 23 y 24

--- python/problema23.py
+++ python/problema23.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-P = [0.1, 5, 10, 20, 25, 30, 40, 45, 50]
-Temp = [350, 400, 450]
-VT350 = [220, 4.1, 2.2, 1.35, 1.1, 0.9, 0.68, 0.61, 0.54]
-VT400 = [250, 4.7, 2.5, 1.49, 1.2, 0.99, 0.75, 0.675, 0.6]
-VT450 = [282.5, 5.23, 2.7, 1.55, 1.24, 1.03, 0.78, 0.7, 0.62]
+P = np.array([0.1, 5, 10, 20, 25, 30, 40, 45, 50])
+Temp = np.array([350, 400, 450])
+VT350 = np.array([220, 4.1, 2.2, 1.35, 1.1, 0.9, 0.68, 0.61, 0.54])
+VT400 = np.array([250, 4.7, 2.5, 1.49, 1.2, 0.99, 0.75, 0.675, 0.6])
+VT450 = np.array([282.5, 5.23, 2.7, 1.55, 1.24, 1.03, 0.78, 0.7, 0.62])
+
 Sxy = 0
 Sx = 0
 Sy = 0
@@ -18,12 +19,12 @@ Sx2y = 0
 Sx3y = 0
 n = 9
 Bet = np.zeros(n)
-for i in range(1,n):
-    Bet[i] = ((VT450[i] - VT350[i]) / (Temp[3] - Temp[1])) / (VT400[i])
+for i in range(0,n):
+    Bet[i] = ((VT450[i] - VT350[i]) / (Temp[2] - Temp[0])) / (VT400[i])
 
 V = np.zeros(n)
-for i in range(1,n):
-    V[i] = VT400[i] * (1 - Temp[2] * Bet[i])
+for i in range(0,n):
+    V[i] = VT400[i] * (1 - Temp[1] * Bet[i])
     Sxy = Sxy + (P[i] * V[i])
     Sx = Sx + P[i]
     Sy = Sy + V[i]
@@ -41,23 +42,23 @@ xp = Sx / n
 A = [[n, Sx, Sx2, Sx3],[Sx, Sx2, Sx3, Sx4],[Sx2, Sx3, Sx4, Sx5],[Sx3, Sx4, Sx5, Sx6]]
 B = [[Sy],[Sxy],[Sx2y],[Sx3y]]
 C = np.dot(np.linalg.inv(A), B)
-a = P[1]
-b = P[9]
+a = P[0]
+b = P[8]
 N = 11
 N2 = N - 1
 h = (b - a) / N2
 mult3 = 0
 part2 = 0
-fa = C[4] * (a**3) + C[3] * (a**2) + C[2] * (a) + C[1]
-fb = C[4] * (b**3) + C[3] * (b**2) + C[2] * (b) + C[1]
+fa = np.array(C[3] * (a**2) + C[2] * (a**2) + C[1] * (a) + C[0])
+fb = np.array(C[3] * (b**2) + C[2] * (b**2) + C[1] * (b) + C[0])
 ff = np.zeros(N2)
 x = a
-for i in range(1,N2):
+for i in range(0,N2):
     x = x + h
-    ff[i] = C[4] * (x**3) + C[3] * (x**2) + C[2] * (x) + C[1]
+    ff[i] = np.array(C[3] * (x**3) + C[2] * (x**2) + C[1] * (x) + C[0])
     print("Iteración: ", i, ", X= ", x, ", f(x)= ", ff[i])
 
-for i in range(2,1,N2-1):
+for i in range(1,1,N2):
     if i % 3 == 0:
         mult3 = mult3 + ff[i]
     else:
--- python/problema24.py
+++ python/problema24.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [[0],[1],[3]] # cm
-y = [[0.06],[0.32], [0.6]] # g cm**(-3)
+x = np.array([[0],[1],[3]]) # cm
+y = np.array([[0.06],[0.32], [0.6]]) # g cm**(-3)
 plt.plot(x,y,color="red",label="datos")
 plt.grid("on")
 # Ajuste lineal
@@ -12,7 +12,7 @@ xi = 0
 yi = 0
 xi2 = 0
 # sumatorias necesarias para el método
-for i in range(1,n):
+for i in range(0,n):
     xyi = xyi + (x[i] * y[i])
     xi = xi + x[i]
     yi = yi + y[i]
@@ -22,14 +22,15 @@ a1 = (n * xyi - xi * yi) / (n * xi2 - xi**2)
 xp = xi / n
 yp = yi / n
 a0 = yp - a1 * xp
-print("a0= ",a0," a1= ",a1 )
+print("a0= ",a0)
+print("a1= ",a1)
 # Generar la función obtenida y graficarla
 f = np.zeros(n)
-for i in range(1,n):
+for i in range(0,n):
     f[i] = a1 * x[i] + a0
 
 plt.plot(x,f,color="green",label="funcion")
-plt.leg(bbox_to_anchor=(0.1, 1), loc=2)
+plt.legend(bbox_to_anchor=(0.1, 1), loc=2)
 # Cálculos adicionales
 D = 1.52 * 10**(-6) # cm**2 s**-1
 A = 3.6 * 10**(6) # m**2
