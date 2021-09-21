b8a32d803ede17a752df04041e2211a6cd007994
Alberto Valencia <rdz.val.alberto@gmail.com>
2021-09-21 13:28:20 -0500

    Ejercicio 25 terminado

--- python/problema25.py
+++ python/problema25.py
@@ -18,11 +18,11 @@ while ep > 0.001:
     Zvs = 1 + B - (q * B) * ((Z0vs - B) / (Z0vs * (Z0vs + B)))
     ep = abs((Zvs - Z0vs) / Zvs) * 100
     i = i + 1
-    print("Número de iteración ", i, " , Z= ", Zvs, ", ep= ", ep)
+    print("Número de iteración "+ str(i)+" , Z= "+ str(Zvs)+ ", ep= "+ str(ep))
     Z0vs = Zvs
 
 print
-print("El factor de compresibilidad para vapor saturado ",Zvs,", con un error relativo porcentual de: ",ep,"\n")
+print("El factor de compresibilidad para vapor saturado "+ str(Zvs)+", con un error relativo porcentual de: "+ str(ep))
 print
 print("Líquido saturado")
 print
@@ -33,8 +33,8 @@ while ep > 0.001:
     Zls = B + ((Z0ls * (Z0ls + B) * (1 + B - Z0ls)) / (q * B))
     ep = abs((Zls - Z0ls) / Zls) * 100
     i = i + 1
-    print("Número de iteración ", i, ", Z= ", Zls, ", ep= ", ep)
+    print("Número de iteración "+ str(i) + ", Z= "+ str(Zls)+ ", ep= "+ str(ep))
     Z0ls = Zls
 
 print
-print("El factor de compresibilidad para liquido saturado ",Zls,", con un error relativo porcentual de: ",ep)
+print("El factor de compresibilidad para liquido saturado "+ str(Zls) +", con un error relativo porcentual de: "+ str(ep))
